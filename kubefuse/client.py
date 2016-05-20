291cd3717f081efdc917c10b0f33947efca6c086
Bart Spaans <bart.spaans@opencredo.com>
2016-05-20 14:56:43 +0100

    add caching to describe, logs, json and yaml files

--- kubefuse/client.py
+++ kubefuse/client.py
@@ -25,13 +25,29 @@ class KubernetesClient(object):
         return result
         
     def get_namespaces(self):
-        return self._load_from_cache_or_do("namespaces", self._get_namespaces)
+        key = "namespaces"
+        cb = self._get_namespaces
+        return self._load_from_cache_or_do(key, cb)
 
-    def _get_namespaces(self):
-        payload = self._run_command_and_parse_yaml('get ns -o yaml'.split())
-        if payload is None or 'items' not in payload:
-            return []
-        return [ item['metadata']['name'] for item in payload['items']]
+    def get_entities(self, ns, entity):
+        key = "%s.%s" % (ns, entity)
+        cb = lambda: self._get_entities(ns,entity)
+        return self._load_from_cache_or_do(key, cb)
+
+    def get_object_in_format(self, ns, entity_type, object, format):
+        key = "%s.%s.%s.%s" % (ns, entity_type, object, format)
+        cb = lambda: self._get_object_in_format(ns, entity_type, object, format)
+        return self._load_from_cache_or_do(key, cb)
+
+    def describe(self, ns, entity_type, object):
+        key = "%s.%s.%s.describe" % (ns, entity_type, object)
+        cb = lambda: self._describe(ns, entity_type, object)
+        return self._load_from_cache_or_do(key, cb)
+
+    def logs(self, ns, object):
+        key = "%s.pod.%s.logs" % (ns, object)
+        cb = lambda: self._logs(ns, object)
+        return self._load_from_cache_or_do(key, cb)
 
     def get_pods(self, ns='default'):
         return self.get_entities(ns, 'pod')
@@ -40,9 +56,12 @@ class KubernetesClient(object):
     def get_replication_controllers(self, ns='default'):
         return self.get_entities(ns, 'rc')
 
-    def get_entities(self, ns, entity):
-        return self._load_from_cache_or_do(ns + "-" + entity, 
-                lambda: self._get_entities(ns,entity))
+
+    def _get_namespaces(self):
+        payload = self._run_command_and_parse_yaml('get ns -o yaml'.split())
+        if payload is None or 'items' not in payload:
+            return []
+        return [ item['metadata']['name'] for item in payload['items']]
 
     def _get_entities(self, ns, entity):
         payload = self._run_command_and_parse_yaml(['get', entity, '-o', 'yaml'] + self._namespace(ns))
@@ -50,12 +69,12 @@ class KubernetesClient(object):
             return []
         return [ item['metadata']['name'] for item in payload['items']]
 
-    def get_object_in_format(self, ns, entity_type, object, format):
+    def _get_object_in_format(self, ns, entity_type, object, format):
         return self._run_command(['get', entity_type, object, '-o', format] + self._namespace(ns))
 
-    def describe(self, ns, entity_type, object):
+    def _describe(self, ns, entity_type, object):
         return self._run_command(['describe', entity_type, object] + self._namespace(ns))
 
-    def logs(self, ns, object):
+    def _logs(self, ns, object):
         return self._run_command(['logs', object] + self._namespace(ns))
 
