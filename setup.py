eb4b92fe42817f4108f3c61ceb23dd67f78df45d
Bart Spaans <bart.spaans@opencredo.com>
2016-05-20 15:13:47 +0100

    release v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "kubefuse",
-    version = "0.3.2",
+    version = "0.4.0",
     packages = find_packages(),
     author = "Bart Spaans",
     author_email = "bart.spaans@gmail.com",
