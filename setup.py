2bd6e8f9280fae93109f5d612421afb9d2a847be
Bart Spaans <bart.spaans@opencredo.com>
2016-08-10 15:16:52 +0100

    Release v0.6

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "kubefuse",
-    version = "0.5.1",
+    version = "0.6.0",
     packages = find_packages(),
     author = "Bart Spaans",
     author_email = "bart.spaans@gmail.com",
     url = "https://github.com/bspaans/kubefuse",
     description = "A FUSE file system for Kubernetes",
     classifiers = [
-	    'Programming Language :: Python',
+        'Programming Language :: Python',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: System',
         'Topic :: System :: Clustering',
@@ -17,8 +17,8 @@ setup(
     ],
     license="Apache License v2",
     entry_points = {
-	    'console_scripts': [
-		    'kubefuse = kubefuse.kubefuse:main'
-	    ]
+        'console_scripts': [
+            'kubefuse = kubefuse.kubefuse:main'
+        ]
     }
  )
